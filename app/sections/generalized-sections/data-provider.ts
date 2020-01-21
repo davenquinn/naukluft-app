fcf2e2afd574379fcf245dce72b7ff6de3f2cdf1
Daven Quinn <dev@davenquinn.com>
2020-01-21 14:25:38 -0600

    Break ordering seems to work correctly

--- app/sections/generalized-sections/data-provider.ts
+++ app/sections/generalized-sections/data-provider.ts
@@ -18,7 +18,7 @@ const GeneralizedDivisionsProvider = (props)=>{
   Provides all surfaces used in Summary Sections diagram
   */
   const {children} = props
-  const divisions = useQuery(sql)
+  const divisions = useQuery(sql) ?? []
 
   const {divisions: allDivisions} = useContext(ColumnDivisionsContext)
   const breaks = useQuery<GeneralizedBreak[]>(breakQuery)
@@ -31,15 +31,34 @@ const GeneralizedDivisionsProvider = (props)=>{
 
   Array.from(groupedBreaks).map(entry => {
     // Do for each group of Sections
-    const [locality, breaks] = entry
-    for (const b in breaks) {
-      console.log(b)
+    let breaks: GeneralizedBreak[] = entry[1]
+    let items: GeneralizedBreak[]
+    let nextBreak: GeneralizedBreak
+
+    nextBreak = breaks.shift()
+    let orderedBreaks = [nextBreak]
+    while (breaks.length > 0) {
+      let ix = breaks.findIndex(d => d.lower_section == orderedBreaks[orderedBreaks.length-1].upper_section)
+      if (ix != -1) {
+        items = breaks.splice(ix,1)
+        orderedBreaks.push(...items)
+        continue
+      }
+      ix = breaks.findIndex(d => d.upper_section == orderedBreaks[0].lower_section)
+      if (ix != -1) {
+        items = breaks.splice(ix,1)
+        orderedBreaks.unshift(...items)
+        continue
+      }
+      throw "Section breaks must be defined by 'upper_section', 'lower_section' pairs"
     }
+
+    console.log(orderedBreaks)
+
   })
 
-  debugger
 
-  console.log(allDivisions, divisions, breaks)
+  //console.log(allDivisions, divisions, breaks)
   return h(ColumnDivisionsContext.Provider, {value: {divisions}}, children)
 }
 
