400366ef966a5f575acb12eeb58a5598688cf4e9
Daven Quinn <dev@davenquinn.com>
2020-02-28 18:32:02 -0600

    Fixed legend

--- app/sections/summary-sections/legend.ts
+++ app/sections/summary-sections/legend.ts
@@ -1,87 +1,78 @@
-import {Component} from "react";
+import {useContext} from "react";
 import h from "react-hyperscript";
 import classNames from "classnames";
-import {SymbolLegend} from "@macrostrat/column-components/dist/esm/symbol-column";
-import {FaciesContext} from "@macrostrat/column-components";
-import {FaciesDescriptionSmall, FaciesSwatch} from "@macrostrat/column-components/dist/esm/editor/facies";
+import {FaciesContext, SymbolLegend, FaciesSwatch} from "@macrostrat/column-components";
 
-class FaciesLegend extends Component {
-  static initClass() {
-    this.defaultProps = {
-      facies: [],
-      onChanged() {},
-      isEditable: true
-    };
-  }
-  render() {
-    return h('div.legend-inner', [
-      h('div.facies-description', [
-        h('h2', 'Sedimentary facies'),
-        h('div.facies-description-inner', [
-          h('div.section', [
-            h('h4', 'Siliciclastic'),
-            this.facies("coarse-clastics", "Coarse sandstone and pebble conglomerate"),
-            this.facies("shallow-fine-clastics", "Inner shoreface sandstone–siltstone"),
-            this.facies("fine-clastics", "Outer shoreface sandstone–mudstone")
-          ]),
-          h('div.section', [
-            h('h4', 'Carbonate'),
-            this.facies("knobbly-stromatolites", "Stromatolite-colonized reworking surface*"),
-            this.facies("carbonate-mudstone"),
-            this.facies("intraclast-grainstone"),
-            this.facies("hcs-grainstone", "Cross-stratified grainstone"),
-            this.facies("mixed-grainstone", 'Wavy-bedded heterolithic'),
-            this.facies("intraclast-breccia", 'Intraclast breccia'),
-            // h('p.explanation', [
-            //   h("span.key", "*"),
-            //   h("span.description", "Not stratigraphically continuous")
-            // ])
-          ])
-        ])
-      ]),
-      h('div.symbol-legend', [
-        h('h2', 'Symbology'),
-        h(SymbolLegend),
-        h('p.note', 'Triangle bars represent variation in accomodation space at the parasequence set level')
-      ])
-    ]);
-  }
+const Facies = (props)=>{
+  const {facies} = useContext(FaciesContext)
+  const {id, selected, children} =  props
 
-  facies(id, title=null){
-    const {selected, facies} = this.props;
-    const d = facies.find(d => d.id === id);
-    if ((d == null)) { return null; }
-    const style = {};
-    if (selected === d.id) {
-      style.backgroundColor = d.color;
-      style.color = 'white';
-    }
-    const className = classNames({selected: selected === d.id});
-
-    return h('div.facies', {
-      key: d.id, style, className
-    }, [
-      h('div.header', [
-        h('p.name', title || d.name),
-        h(FaciesSwatch, {facies: d})
-      ])
-    ]);
+  const d = facies.find(d => d.id === id);
+  if ((d == null)) { return null; }
+  const style = {};
+  if (selected === d.id) {
+    style.backgroundColor = d.color;
+    style.color = 'white';
   }
+  const className = classNames({selected: selected === d.id});
+
+  return h('div.facies', {
+    key: d.id, style, className
+  }, [
+    h('div.header', [
+      h('p.name', children || d.name),
+      h(FaciesSwatch, {facies: d})
+    ])
+  ]);
 }
-FaciesLegend.initClass();
 
-class Legend extends Component {
-  render() {
-    return h('div.legend#summary-sections-legend', [
-      h("h1", [
-        h("span", "Zebra Nappe"),
-        " stratigraphic model"
-      ]),
-      h(FaciesContext.Consumer, null, props=> {
-        return h(FaciesLegend, props);
-      })
-    ]);
-  }
+const LegendInner = (props)=>{
+  return h('div.legend-inner', [
+    h('div.facies-description', [
+      h('h2', 'Sedimentary facies'),
+      h('div.facies-description-inner', [
+        h('div.section', [
+          h('h4', 'Siliciclastic'),
+          h(Facies, {id: "coarse-clastics"}, "Coarse sandstone and pebble conglomerate"),
+          h(Facies, {id: "shallow-fine-clastics"}, "Inner shoreface sandstone–siltstone"),
+          h(Facies, {id: "fine-clastics"}, "Outer shoreface sandstone–mudstone")
+        ]),
+        h('div.section', [
+          h('h4', 'Carbonate'),
+          h(Facies, {id: "knobbly-stromatolites"}, "Stromatolite-colonized reworking surface*"),
+          h(Facies, {id: "carbonate-mudstone"}),
+          h(Facies, {id: "intraclast-grainstone"}),
+          h(Facies, {id: "hcs-grainstone"}, "Cross-stratified grainstone"),
+          h(Facies, {id: "mixed-grainstone"}, 'Wavy-bedded heterolithic'),
+          h(Facies, {id: "intraclast-breccia"}, 'Intraclast breccia'),
+          // h('p.explanation', [
+          //   h("span.key", "*"),
+          //   h("span.description", "Not stratigraphically continuous")
+          // ])
+        ])
+      ])
+    ]),
+    h('div.symbol-legend', [
+      h('h2', 'Symbology'),
+      h(SymbolLegend),
+      h('p.note', 'Triangle bars represent variation in accomodation space at the parasequence set level')
+    ])
+  ]);
+}
+
+LegendInner.defaultProps = {
+  onChanged() {},
+  isEditable: true
+};
+
+const Legend = ()=> {
+  return h('div.legend#summary-sections-legend', [
+    h("h1", [
+      h("span", "Zebra Nappe"),
+      " stratigraphic model"
+    ]),
+    h(LegendInner)
+  ]);
 }
 
 export {Legend};
