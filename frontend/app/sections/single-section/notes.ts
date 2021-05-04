53daec7e1fe44a5f86bb0e570031d65d8240a7a8
Daven Quinn <dev@davenquinn.com>
2021-05-04 00:00:06 -0500

    Fixed types bundling

--- frontend/app/sections/single-section/notes.ts
+++ frontend/app/sections/single-section/notes.ts
@@ -1,9 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import { format } from "d3-format";
 import { useState, useContext } from "react";
 import h from "@macrostrat/hyper";
