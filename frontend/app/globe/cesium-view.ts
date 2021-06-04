e9e980d6b4aa08fb7c083e7d15f0ee79791bf113
Daven Quinn <dev@davenquinn.com>
2021-06-03 23:03:15 -0500

    Added photogrammetry

--- frontend/app/globe/cesium-view.ts
+++ frontend/app/globe/cesium-view.ts
@@ -1,10 +1,12 @@
 import CesiumView, {
   DisplayQuality,
   buildPositionHash,
-  getInitialPosition
+  getInitialPosition,
+  useCesium
 } from "@macrostrat/cesium-viewer";
 import { getHashString, setHashString } from "@macrostrat/ui-components";
 import h from "@macrostrat/hyper";
+import { IonResource, Ion } from "cesium";
 import { useDispatch, useSelector } from "react-redux";
 import { queryMap, mapMoved } from "../actions";
 import {
@@ -17,9 +19,10 @@ import {
   SatelliteLayer,
   terrainProvider
 } from "@macrostrat/cesium-viewer/layers";
-import { ImageryLayer } from "resium";
+import { Cesium3DTileset } from "resium";
 import { useMemo } from "react";
 import MVTImageryProvider from "mvt-imagery-provider";
+import { useEffect } from "react";
 
 /*
 import { mapStyle } from "./vector-style";
@@ -38,6 +41,9 @@ const GeologyLayer = ({ visibleMaps = null, ...rest }) => {
 };
 */
 
+Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzODk2OGM4ZS1mMzlkLTRlNjAtYWQxZS1mODU3YWJjMWFhNzQiLCJpZCI6MjYwODYsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1ODcxOTU1MTh9._ILy51LI2aF7Nxvas9RQDkhqOP4Tp92uTYAtvewVvNE";
+
 const defaultPosition = getInitialPosition({
   a: 314,
   e: 67,
@@ -47,6 +53,19 @@ const defaultPosition = getInitialPosition({
 });
 
 const initialPosition = getInitialPosition(getHashString(), defaultPosition);
+
+// function NeurasCesiumTileset() {
+//   const { viewer } = useCesium();
+//   //   console.log("Adding 3d tileset", viewer);
+//     var tileset = viewer?.scene.primitives.add(
+//       new Cesium3DTileset({
+//         url: "http://localhost:3002/tilesets/Neuras/tileset.json"
+//       })
+//     );
+//   }, [viewer]);
+
+//   return null;
+// }
 
 function NaukluftCesiumView(props) {
   return h(
@@ -59,12 +78,25 @@ function NaukluftCesiumView(props) {
         console.log(latitude, longitude);
       },
       initialPosition,
-      terrainExaggeration: 1,
-      displayQuality: DisplayQuality.High,
-      showInspector: false,
+      //terrainExaggeration: 1,
+      //displayQuality: DisplayQuality.High,
+      showInspector: true,
       terrainProvider
     },
-    [h(HillshadeLayer), h(SatelliteLayer)]
+    [
+      h(Cesium3DTileset, {
+        url: "http://localhost:3002/tilesets/Neuras/tileset.json",
+        //url: IonResource.fromAssetId(75343),
+        onReady(tileset) {
+          console.log(tileset);
+        },
+        show: true,
+        maximumScreenSpaceError: 2
+      }),
+      //h(NeurasCesiumTileset),
+      //h(HillshadeLayer),
+      h(SatelliteLayer)
+    ]
     //, h(GeologyLayer, { alpha: 0.8 })]
   );
 }
