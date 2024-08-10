08bfe3689027e67c6baebe47605cdddf6fd9d903
Daven Quinn <code@davenquinn.com>
2024-08-10 06:50:32 -0500

    Start adjusting for new infrastructure

--- frontend/scripts/deploy-frontend.zsh
+++ frontend/scripts/deploy-frontend.zsh
@@ -8,10 +8,10 @@ rsync -av "$patterns/" "$dist/lithology-patterns/"
 rsync -av "assets/column-patterns/" "$dist/column-symbols/"
 
 export NODE_ENV=production
-export PUBLIC_PATH="/naukluft/"
-export NAUKLUFT_API_BASE_URL="https://dev.macrostrat.org/naukluft/api"
+export BUCKET_NAME="macrostrat-storage:macrostrat-sites"
+export PUBLIC_PATH="/"
+export NAUKLUFT_API_BASE_URL="https://naukluft.svc.macrostrat.org/api"
 webpack
 
 # Rclone to S3
-rclone sync --copy-links --progress \
-  $dist/ macrostrat-s3:macrostrat-sites/naukluft/
+rclone sync --copy-links --progress $dist/ $BUCKET_NAME/naukluft/
